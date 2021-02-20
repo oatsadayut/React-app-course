2c77e6c68bd8ca8680fa0b0881f8edf448ab9179
ice <icelove.bybyby@gmail.com>
2021-02-20 18:43:42 +0700

    Add Component User

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import PropTypes from 'prop-types';
+import User from './user';
+
 
 // prop
 const Footer = ({name,year,phone,isOpen,age}) => {
     return (
         <div>
             <h1>Footer : {name} :: {year} Age:: {age} Tel. {phone} Online : {isOpen.toString()}</h1>
+            <User />
         </div>
     )
 }
