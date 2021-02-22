13ec61bf182f24dd59c38554d9110796178d526c
ice <icelove.bybyby@gmail.com>
2021-02-22 22:19:38 +0700

    Toggle useState

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -4,18 +4,28 @@ const Sidebar = () => {
 
 // useState ************* //
     const [name,setName] = useState('')
+    const [isShow , setIsShow] = useState(true) //Boolean
+
     const click =()=>{
         setName('อัษฎายุธ')
     }
     const unclick =()=>{
         setName('')
     }
+    //Toggle True And False
+    const clickToggle =()=>{
+        setIsShow(!isShow)
+    }
 // ********************** //
     return (
         <div>
                <h1>ชื่อ : {name}</h1>
+
+                {isShow ? <p>True</p> : <p>False</p>}
+
                <button onClick={click}> Click เพิ่มชื่อ</button> 
                <button onClick={unclick}> Click ลบชื่อ</button> 
+               <button onClick={clickToggle}> TOGGLE </button> 
 
         </div>
     )
